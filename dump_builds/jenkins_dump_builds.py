1fc938e372e73a87ba70a70066352b400286ad26
Guillaume Quéré <none>
2020-02-13 10:52:32 +0100

    [fix] Bunch of fixes to jenkins_dump_builds
    - Gracefully handle errors
    - Rewrite URLs if API is retarded

--- dump_builds/jenkins_dump_builds.py
+++ dump_builds/jenkins_dump_builds.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import urllib3
+from urllib.parse import urlparse
 import os
 import argparse
 import concurrent.futures
@@ -39,8 +40,10 @@ def create_dir(path):
 
 # SAVERS #######################################################################
 def dump_to_disk(url, consoleText, envVars):
+
     # first, need to create dirs
-    folder = OUTPUT_DIR + url.replace(BASE_URL, '')
+    parsed_url = urlparse(url)
+    folder = OUTPUT_DIR + '/' + parsed_url.path
     create_dir(folder)
 
     # then dump files
@@ -57,10 +60,29 @@ def job_was_dumped(url):
 
 
 # DUMPERS ######################################################################
+# some retarded Jenkinses return 'localhost' or some other crap instead of a
+# FQDN when queried via the API, so these have to be replaced each time
+def sanitize_url(url):
+    parsed_url = urlparse(url)
+    parsed_original_url = urlparse(BASE_URL)
+    sanitized = parsed_url._replace(netloc=parsed_original_url.netloc)
+    return sanitized.geturl()
+
+
 def dump_jobs(url):
-    r = SESSION.get(url + '/api/json/', verify=False, auth=AUTH, timeout=20)
+    url = sanitize_url(url)
+
+    try:
+        r = SESSION.get(url + '/api/json/', verify=False, auth=AUTH, timeout=20)
+    except requests.exceptions.SSLError as e:
+        print('[ERROR] This server refused our key negociation, retry with -d to downgrade ciphers')
+        exit(1)
+    except requests.exceptions.ConnectTimeout:
+        print('[ERROR] Connection to the server timed out. Is the server up?')
+        exit(1)
+
     if 'Authentication required' in r.text:
-        print('[ERROR] This Jenkins needs authentication')
+        print('[ERROR] This Jenkins requires authentication')
         exit(1)
     if 'Invalid password/token' in r.text:
         print('[ERROR] Invalid password/token for user')
@@ -69,8 +91,11 @@ def dump_jobs(url):
         print('[ERROR] User has no read permission')
         exit(1)
 
-    response = json.loads(r.text)
-    print_debug(response)
+    try:
+        response = json.loads(r.text)
+    except:
+        print('[ERROR] Failed parsing server response for {}'.format(url + '/api/json/'))
+        return
 
     if 'jobs' in response:
         for job in response['jobs']:
@@ -89,6 +114,8 @@ def dump_jobs(url):
 
 
 def dump_build(url):
+    print_debug('    Dumping build {}'.format(url))
+    url = sanitize_url(url)
     r = SESSION.get(url + '/consoleText', verify=False, auth=AUTH, timeout=20)
     consoleText = r.text
     r = SESSION.get(url + '/injectedEnvVars/api/json', verify=False, auth=AUTH, timeout=20)
@@ -104,16 +131,17 @@ parser.add_argument('url', nargs='+', type=str)
 parser.add_argument('-u', '--user', type=str)
 parser.add_argument('-p', '--password', type=str)
 parser.add_argument('-o', '--output-dir', type=str)
-parser.add_argument('-d', '--downgrade_ssl', action='store_true', help='Downgrade SSL to use RSA')
 parser.add_argument('-l', '--last', action='store_true', help='Dump only the last build of each job')
 parser.add_argument('-r', '--recover_from_failure', action='store_true', help='Recover from server failure, skip all existing directories')
+parser.add_argument('-d', '--downgrade_ssl', action='store_true', help='Downgrade SSL to use RSA (for legacy)')
+parser.add_argument('-s', '--no_use_session', action='store_true', help='Don\'t reuse the HTTP session, but create a new one for each request (for legacy)')
+parser.add_argument('-v', '--verbose', action='store_true', help='Debug mode')
 
 args = parser.parse_args()
 if args.user and args.password:
     AUTH = (args.user, args.password)
 else:
     AUTH = None
-BASE_URL = args.url[0]
 if args.output_dir:
     OUTPUT_DIR = args.output_dir + '/'
 if args.downgrade_ssl:
@@ -122,12 +150,17 @@ if args.last:
     RECOVER_LAST_BUILD_ONLY = True
 if args.recover_from_failure:
     RECOVER_FROM_FAILURE = True
-
-SESSION = requests.session()
+if args.verbose:
+    DEBUG = True
+if args.no_use_session:
+    SESSION = requests
+else:
+    SESSION = requests.session()
+BASE_URL = args.url[0]
 
 print('[+] Getting a list of jobs and builds')
 dump_jobs(BASE_URL)
-
+print('    Got {} builds to dump'.format(len(BUILD_LIST)))
 print('[+] Dumping gathered builds')
 with concurrent.futures.ThreadPoolExecutor(max_workers=128) as executor:
     executor.map(dump_build, BUILD_LIST)
